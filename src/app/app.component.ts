d449f80445e3fc544182525fa8c0b794a56ddf41
Javier <gutifer666@gmail.com>
2025-05-28 11:59:31 +0200

    add SideNavComponent with responsive Angular Material layout and integrate into AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {SideNavComponent} from './shared/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
